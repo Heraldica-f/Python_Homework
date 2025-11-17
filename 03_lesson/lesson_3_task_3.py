198890107275edd8b561e813ae05e23c6ee1003e
Heraldica-f <TourusGoya@yandex.ru>
2025-11-17 22:13:41 +0300

    Lesson_3

--- 03_lesson/lesson_3_task_3.py
+++ 03_lesson/lesson_3_task_3.py
@@ -1,9 +1,9 @@
-from adress import Adress
+from address import Address
 from mailing import Mailing
 
-to_adress = Adress('123456','Москва','Лесная','18Б','145')
-from_adress = Adress('654321','Чебоксары','Разведчика Абеля','12к3','64')
+to_address = Address('123456','Москва','Лесная','18Б','145')
+from_address = Address('654321','Чебоксары','Разведчика Абеля','12к3','64')
 
-delivery = Mailing(to_adress, from_adress,'1648','147258TRACK')
+delivery = Mailing(to_address, from_address,'1648','147258TRACK')
 
 print(delivery)
